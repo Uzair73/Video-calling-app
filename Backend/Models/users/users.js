cf2088f9bd0a6e31b89ce2a924ae89f061e7d203
uzair73 <uzairhakeem71@gmail.com>
2024-11-19 15:15:09 +0500

    register api done

--- Backend/Models/users/users.js
+++ Backend/Models/users/users.js
@@ -10,4 +10,10 @@ const create_user = async ({username, user_password, email}) => {
   return res.rows[0];
 };
 
-module.exports = { create_user };
+
+// find the user by email
+const find_user_by_email = async (email) => {
+  const res = await db.query('SELECT * FROM users WHERE email = $1', [email])
+  return res.rows[0];
+}
+module.exports = { create_user, find_user_by_email };
--- Backend/controllers/users/Auth/signup.js
+++ Backend/controllers/users/Auth/signup.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { create_user } = require('../../../Models/users/users');
+const { create_user, find_user_by_email } = require('../../../Models/users/users');
 require('dotenv').config();
 
 // register endpoint
@@ -8,16 +8,25 @@ const signUp = async (req, res) => {
         return res.status(400).json("Bad Request");
     }
     const {username, email, user_password} = req.body;
+
+    // validate the user input
+    if(!req.body.username || !req.body.email || !req.body.user_password){
+        return res.status(400).json("Bad Request: All fields are required");
+    }
+
     try {
-        const user_create = await create_user({username, email, user_password});
-        if(username === '' || email == '' || user_password == ''){
-            return res.status(400).json("Please fill in all fields");
+        const same_email_check = await find_user_by_email(email);
+        console.log("same email", same_email_check);
+        
+        if(same_email_check){
+            return res.status(409).json("Email already exists");
         }
+        const user_create = await create_user({username, email, user_password});
         res.status(201).json(user_create);
         console.log("User was successfully registered", user_create);
-    } catch (error) {
+        } catch (error) {
         console.log("Error occurred in the register endpoint", error);
         res.status(500).json({error: "Internal server error"});
-    }
+        }
 }
 module.exports = signUp;
