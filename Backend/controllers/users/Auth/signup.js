0275a4c9869caa4563627f900e8b113c2dced915
uzair73 <uzairhakeem71@gmail.com>
2024-11-19 15:44:39 +0500

    add the token in register api

--- Backend/controllers/users/Auth/signup.js
+++ Backend/controllers/users/Auth/signup.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const { create_user, find_user_by_email } = require('../../../Models/users/users');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 // register endpoint
 const signUp = async (req, res) => {
-    if(!req.body){
-        return res.status(400).json("Bad Request");
+    if (!req.body) {
+        return res.status(400).json({ message: "Bad Request", success: false });
     }
-    const {username, email, user_password} = req.body;
+    const { username, email, user_password } = req.body;
 
     // validate the user input
-    if(!req.body.username || !req.body.email || !req.body.user_password){
-        return res.status(400).json("Bad Request: All fields are required");
+    if (!username || !email || !user_password) {
+        return res.status(400).json({ message: "Bad Request: All fields are required", success: false });
     }
 
     try {
         const same_email_check = await find_user_by_email(email);
-        console.log("same email", same_email_check);
-        
-        if(same_email_check){
-            return res.status(409).json("Email already exists");
+        if (same_email_check) {
+            return res.status(409).json({ message: "Email already exists", success: false });
         }
-        const user_create = await create_user({username, email, user_password});
-        res.status(201).json(user_create);
-        console.log("User was successfully registered", user_create);
-        } catch (error) {
+        const user_create = await create_user({ username, email, user_password });
+        const payload = {
+            user: {
+                id: user_create.id
+            }
+        };
+        const token = jwt.sign(payload, process.env.TOKEN_SECREAT, { expiresIn: '24h' });
+        return res.status(201).json({ message: "User was successfully registered", token, success: true });
+    } catch (error) {
         console.log("Error occurred in the register endpoint", error);
-        res.status(500).json({error: "Internal server error"});
-        }
+        res.status(500).json({ message: "Internal server error", success: false });
+    }
 }
 module.exports = signUp;
