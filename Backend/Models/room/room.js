549abaa3d047a4403e566e68a7acbc9b5e0903f9
uzair73 <uzairhakeem71@gmail.com>
2024-11-20 22:38:06 +0500

    added the fetch room api

--- Backend/Models/room/room.js
+++ Backend/Models/room/room.js
@@ -6,4 +6,10 @@ const create_room = async ({room_name, created_by, room_type})=>{
     return res.rows[0]
 }
 
-module.exports = { create_room }
+// fetch the room details
+const fetch_room = async (id)=>{
+    const res = await db.query('SELECT * FROM rooms WHERE id = $1', [id])
+    return res.rows[0]
+}
+
+module.exports = { create_room , fetch_room }
--- Backend/routes/room_routes.js
+++ Backend/routes/room_routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const authenticate_token = require('../middleware/auth_middleware');
 
 const create_room = require('../controllers/rooms/creating_room')
+const fetch_room = require('../controllers/rooms/fetch_rooms')
 
 
 router.post('/user-room',authenticate_token, create_room);
+router.get('/fetch-room/:id',authenticate_token, fetch_room);
 
 
 
