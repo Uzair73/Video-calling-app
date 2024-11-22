39c556f9fba62ee546ceb8aab3baa842db042072
uzair73 <uzairhakeem71@gmail.com>
2024-11-22 12:25:08 +0500

    remove participant api done

--- Backend/Models/room-participant/participant.js
+++ Backend/Models/room-participant/participant.js
@@ -6,4 +6,10 @@ const rooom_participant = async (user_id,room_id)=>{
     return res.rows[0]
 }
 
-module.exports = {rooom_participant}
+
+// remove the participant to the room
+const remove_participant = async (room_id, user_id)=>{
+    const res = db.query('DELETE FROM room_participants WHERE room_id = $1 AND user_id = $2', [room_id,user_id])
+    return res
+}
+module.exports = {rooom_participant, remove_participant}
--- Backend/routes/room_participant.js
+++ Backend/routes/room_participant.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const authenticate_token = require('../middleware/auth_middleware');
 
 const add_participant = require('../controllers/room-participant/participant')
+const remove_participant = require('../controllers/room-participant/remove_participant')
 
 router.post('/rooms/:roomId/participants', authenticate_token, add_participant)
+router.delete('/rooms/:roomId/participants/:userId', authenticate_token, remove_participant)
 
 
 module.exports = router;
