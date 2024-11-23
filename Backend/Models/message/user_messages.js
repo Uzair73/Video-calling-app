cbef571e75b1c546aa85cc1e1a3a22443b5f4313
uzair73 <uzairhakeem71@gmail.com>
2024-11-23 11:55:31 +0500

    fetching the messages api done

--- Backend/Models/message/user_messages.js
+++ Backend/Models/message/user_messages.js
@@ -5,4 +5,10 @@ const send_msg = async (room_id, sender_id, content) => {
  const res = await db.query('INSERT INTO message (room_id, sender_id, content) VALUES ($1, $2, $3) RETURNING *', [room_id, sender_id, content])
  return res.rows[0]
 }
-module.exports = {send_msg}
+
+// fetch the messages by the room id
+const fetch_messages= async (room_id) => {
+    const res = await db.query('SELECT * FROM message WHERE room_id = $1 ORDER BY send_at', [room_id])
+    return res.rows
+}
+module.exports = {send_msg, fetch_messages}
--- Backend/routes/user_routes.js
+++ Backend/routes/user_routes.js
@@ -8,6 +8,7 @@ const update_user = require('../controllers/users/update_user')
 const del_user = require('../controllers/users/del_user')
 
 const user_messages = require('../controllers/messages/send_messages')
+const fetch_messages = require('../controllers/messages/fetch_messages')
 router.get('/user-details',authenticate_token, fetch_users);
 router.get('/user/:id',authenticate_token, fetch_user_by_id);
 router.put('/update-user',authenticate_token, update_user);
@@ -15,6 +16,7 @@ router.delete('/del-user/:id',authenticate_token, del_user);
 
 // messages
 router.post('/messages/:room_id', authenticate_token, user_messages)
+router.get('/get-messages/:room_id', authenticate_token, fetch_messages)
 
 
 module.exports = router;
