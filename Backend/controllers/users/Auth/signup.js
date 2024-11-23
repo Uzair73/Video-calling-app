dae21015d5d63ba86c1eaf6d5c43055c5f99441a
uzair73 <uzairhakeem71@gmail.com>
2024-11-23 17:36:12 +0500

    fixing signup & login issues

--- Backend/controllers/users/Auth/signup.js
+++ Backend/controllers/users/Auth/signup.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { create_user, find_user_by_email } = require('../../../Models/users/users');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 // register endpoint
@@ -9,28 +8,20 @@ const signUp = async (req, res) => {
         return res.status(400).json({ success: false , message: "Bad Request: No data in request body"});
     }
     const { username, email, user_password } = req.body;
-
     // validate the user input
     if (!username || !email || !user_password) {
         return res.status(400).json({ success: false , message: "Bad Request: All fields are required"});
     }
-
     try {
         const same_email_check = await find_user_by_email(email);
         if (same_email_check) {
             return res.status(409).json({ success: false , message: "Conflict: Email already exists"});
         }
-        const user_create = await create_user({ username, email, user_password });
-        const payload = {
-            user: {
-                id: user_create.id
-            }
-        };
-        const token = jwt.sign(payload, process.env.TOKEN_SECREAT, { expiresIn: '24h' });
-        return res.status(201).json({ success: true , message: "User was successfully registered", token});
+        await create_user({ username, email, user_password });
+        return res.status(201).json({ success: true , message: "User was successfully registered"});
     } catch (error) {
         console.log("Error occurred in the register endpoint", error);
-        res.status(500).json({ success: false , message: "Internal server error", error:error.message});
+        res.status(500).json({ success: false , message: "Internal server error", error: error.message});
     }
 }
 module.exports = signUp;
